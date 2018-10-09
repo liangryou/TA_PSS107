2aa580a77d245e23408bad7643cf7a84377a194b
Andres <b03302212@ntu.edu.tw>
2018-10-09 21:17:47 +0800

    Add files via upload

---
+++ as1.py
@@ -0,0 +1,25 @@
+a = [{
+'id': 'a13248',
+'district' : '信義區',
+'area' : 18.5,
+'price' : 17890000
+}, {
+'id': 'a13249',
+'district' : '信義區',
+'area' : 21.3,
+'price' : 20000000
+} ,{
+'id': 'a13250',
+'district' : '信義區',
+'area' : 32.1,
+'price' : 36820000
+}]
+print(a)
+print(type(a))
+print(a[1]['price'])
+
+# TODO: use generator to build the series
+#fibonacci
+
+a1 = 1
+a2 = 1
